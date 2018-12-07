f7f260e568248df5176621ff725e348adc77bf68
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-12-07 13:15:11 +0300

    modules

--- src/main/ClassA.java
+++ src/main/ClassA.java
@@ -1,7 +1,20 @@
 package main;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class ClassA {
     public static void main(String[] args) {
         System.out.println("pewpew");
+
+        String s;
     }
-}                                                             
+
+    public static void doit(int no) throws IOException {
+        for (int i = 0; i < no; i++) {
+            String fileName = "name" + i + ".txt";
+            PrintWriter printer = new PrintWriter(fileName, "UTF-8");
+
+        }
+    }
+}
